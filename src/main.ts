d0fc5e83c2ceaf1cfda08d98dd75594a0e92bd64
James Socol <James_Socol@condenast.com>
2021-08-07 20:36:37 -0400

    Update the index page again

--- src/main.ts
+++ src/main.ts
@@ -51,20 +51,45 @@ import {
   ];
 
 
-    const id = I3DXMatrixIdentity(4);
+  const id = I3DXMatrixIdentity(4);
 
-    const triLeftFront = [
-      new I3DXVertex(0, 0, 0, ARGB(0x80, 0, 0, 0xff)),
-      new I3DXVertex(3, 3, 0, ARGB(0x80, 0, 0, 0xff)),
-      new I3DXVertex(6, 0, 0, ARGB(0x80, 0, 0, 0xff)),
-    ];
+  const triLeftFront = [
+    new I3DXVertex(0, 0, 0, ARGB(0x80, 0, 0, 0xff)),
+    new I3DXVertex(3, 3, 0, ARGB(0x80, 0, 0, 0xff)),
+    new I3DXVertex(6, 0, 0, ARGB(0x80, 0, 0, 0xff)),
+  ];
 
-    const triLeftBack = [
-      new I3DXVertex(0, 0, 0, XRGB(0xff, 0, 0)),
-      new I3DXVertex(3, 3, 0, XRGB(0xff, 0, 0)),
-      new I3DXVertex(6, 0, 0, XRGB(0xff, 0, 0)),
-    ];
+  const triLeftBack = [
+    new I3DXVertex(0, 0, 0, XRGB(0xff, 0, 0)),
+    new I3DXVertex(3, 3, 0, XRGB(0xff, 0, 0)),
+    new I3DXVertex(6, 0, 0, XRGB(0xff, 0, 0)),
+  ];
 
+
+  const matView = I3DXMatrixLookToLH(
+    I3DXVector3(camXInput.valueAsNumber, camYInput.valueAsNumber, camZInput.valueAsNumber), // the camera position
+    I3DXVector3(dirXInput.valueAsNumber, dirYInput.valueAsNumber, dirZInput.valueAsNumber), // the "look-to" direction
+    I3DXVector3(0, 1, 0), // the "up" direction
+  );
+  const matProj = I3DXMatrixPerspectiveFovLH(
+    I3DXToRadian(fovyInput.valueAsNumber), // the horizontal field of view
+    WIDTH / HEIGHT, // aspect ratio
+    1.0, // near view-plane
+    100.0, // far view-plane
+  );
+
+  const i3d = new I3DXDevice(container, WIDTH, HEIGHT);
+
+  i3d.SetTransform(I3DTS_VIEW, matView);
+  i3d.SetTransform(I3DTS_PROJECTION, matProj);
+  i3d.SetTransform(I3DTS_WORLD, id);
+
+  let idx = 0;
+  let isPlaying = false;
+  let lastFrameEnd: number;
+  function play() {
+    //console.time('frame');
+    i3d.BeginScene();
 
     const matView = I3DXMatrixLookToLH(
       I3DXVector3(camXInput.valueAsNumber, camYInput.valueAsNumber, camZInput.valueAsNumber), // the camera position
@@ -78,86 +103,9 @@ import {
       100.0, // far view-plane
     );
 
-    let i3d = new I3DXDevice(container, WIDTH, HEIGHT);
-    let idx = 0;
-
     i3d.SetTransform(I3DTS_VIEW, matView);
-    //i3d.SetTransform(I3DTS_PROJECTION, matProj);
     i3d.SetTransform(I3DTS_PROJECTION, matProj);
     i3d.SetTransform(I3DTS_WORLD, id);
-
-    let isPlaying = false;
-    let lastFrameEnd: number;
-    function play() {
-        //console.time('frame');
-        i3d.BeginScene();
-
-        const matView = I3DXMatrixLookToLH(
-          I3DXVector3(camXInput.valueAsNumber, camYInput.valueAsNumber, camZInput.valueAsNumber), // the camera position
-          I3DXVector3(dirXInput.valueAsNumber, dirYInput.valueAsNumber, dirZInput.valueAsNumber), // the "look-to" direction
-          I3DXVector3(0, 1, 0), // the "up" direction
-        );
-        const matProj = I3DXMatrixPerspectiveFovLH(
-          I3DXToRadian(fovyInput.valueAsNumber), // the horizontal field of view
-          WIDTH / HEIGHT, // aspect ratio
-          1.0, // near view-plane
-          100.0, // far view-plane
-        );
-
-        i3d.SetTransform(I3DTS_VIEW, matView);
-        i3d.SetTransform(I3DTS_PROJECTION, matProj);
-        i3d.SetTransform(I3DTS_WORLD, id);
-
-        const blueDepth = blueDepthInput.valueAsNumber;
-        const blueTransform = I3DXTranslateMatrix(-9, 2, blueDepth);
-        i3d.SetTransform(I3DTS_WORLD, blueTransform);
-        i3d.DrawPrimitive(I3DPT_TRIANGLELIST, triLeftFront);
-
-        const redDepth = redDepthInput.valueAsNumber;
-        const redTransform = I3DXTranslateMatrix(-9, 2, redDepth);
-        i3d.SetTransform(I3DTS_WORLD, redTransform);
-        i3d.DrawPrimitive(I3DPT_TRIANGLELIST, triLeftBack);
-
-        const rot = I3DXRotateYMatrix(idx);
-
-        i3d.SetTransform(I3DTS_WORLD, rot);
-        i3d.DrawPrimitive(I3DPT_TRIANGLELIST, movingTriangle);
-
-        const left = I3DXTranslateMatrix(0, 0, Math.sin(idx / 2));
-
-        i3d.SetTransform(I3DTS_WORLD, left);
-        i3d.DrawPrimitive(I3DPT_TRIANGLELIST, fixedTriangle);
-
-        i3d.EndScene();
-        i3d.Present();
-
-        //console.timeEnd('frame');
-        const now = Date.now();
-        const dt = now - lastFrameEnd;
-        idx += 0.005 * dt;
-        if (isPlaying) {
-            requestAnimationFrame(play);
-        }
-        lastFrameEnd = now;
-    }
-
-    window.addEventListener('keyup', function(e) {
-        if(e.keyCode == 27) { // Escape
-            isPlaying = false;
-            console.log("Stop animation.");
-        }
-        else if (e.keyCode == 13) {
-            isPlaying = true;
-            lastFrameEnd = Date.now();
-            console.log("Start animation.");
-            requestAnimationFrame(play);
-        }
-        e.preventDefault();
-        return false;
-    }, true);
-    console.log("Construct scene", new Date());
-    i3d.BeginScene();
-    //i3d.DrawPrimitive(I3DPT_TRIANGLELIST, fixedTriangle);
 
     const blueDepth = blueDepthInput.valueAsNumber;
     const blueTransform = I3DXTranslateMatrix(-9, 2, blueDepth);
@@ -169,7 +117,54 @@ import {
     i3d.SetTransform(I3DTS_WORLD, redTransform);
     i3d.DrawPrimitive(I3DPT_TRIANGLELIST, triLeftBack);
 
+    const rot = I3DXRotateYMatrix(idx);
+
+    i3d.SetTransform(I3DTS_WORLD, rot);
+    i3d.DrawPrimitive(I3DPT_TRIANGLELIST, movingTriangle);
+
+    const left = I3DXTranslateMatrix(0, 0, Math.sin(idx / 2));
+
+    i3d.SetTransform(I3DTS_WORLD, left);
+    i3d.DrawPrimitive(I3DPT_TRIANGLELIST, fixedTriangle);
+
     i3d.EndScene();
     i3d.Present();
-    console.log("End Construct scene", new Date());
+
+    //console.timeEnd('frame');
+    const now = Date.now();
+    const dt = now - lastFrameEnd;
+    idx += 0.005 * dt;
+    if (isPlaying) {
+        requestAnimationFrame(play);
+    }
+    lastFrameEnd = now;
+  }
+
+  window.addEventListener('keyup', function(e) {
+    if (e.keyCode == 13) {
+      isPlaying = !isPlaying;
+      if (isPlaying) {
+        lastFrameEnd = Date.now();
+        requestAnimationFrame(play);
+      }
+    }
+    e.preventDefault();
+    return false;
+  }, true);
+
+  i3d.BeginScene();
+  i3d.DrawPrimitive(I3DPT_TRIANGLELIST, fixedTriangle);
+
+  const blueDepth = blueDepthInput.valueAsNumber;
+  const blueTransform = I3DXTranslateMatrix(-9, 2, blueDepth);
+  i3d.SetTransform(I3DTS_WORLD, blueTransform);
+  i3d.DrawPrimitive(I3DPT_TRIANGLELIST, triLeftFront);
+
+  const redDepth = redDepthInput.valueAsNumber;
+  const redTransform = I3DXTranslateMatrix(-9, 2, redDepth);
+  i3d.SetTransform(I3DTS_WORLD, redTransform);
+  i3d.DrawPrimitive(I3DPT_TRIANGLELIST, triLeftBack);
+
+  i3d.EndScene();
+  i3d.Present();
 })();
